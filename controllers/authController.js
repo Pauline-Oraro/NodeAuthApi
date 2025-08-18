336904301d7b9531d5568cfb1642ecab15a6a4ee
Pauline-Oraro <hiltrapauline@gmail.com>
2025-08-18 17:49:03 +0300

    verification code functionality to authController and router

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 
-const {signupSchema, signinSchema} = require ('../middlewares/validator');
+const {signupSchema, signinSchema, acceptCodeSchema} = require ('../middlewares/validator');
 
 //importing usersModel
 const User = require ('../models/usersModel');
@@ -151,6 +151,74 @@ exports.sendVerificationCode = async (req, res) => {
     }
 
     catch (error){
+        console.log(error);
+    }
+}
+
+
+//verify verification code
+exports.verifyVerificationCode = async (req, res) => {
+    const {email, providedCode} = req.body;
+
+    try{
+        const {error, value} = acceptCodeSchema.validate({email, providedCode});
+        if (error) {
+			return res
+				.status(401)
+				.json({ success: false, message: error.details[0].message });
+		}
+        const codeValue = providedCode.toString();
+        const existingUser = await User.findOne({ email }).select(
+			'+verificationCode +verificationCodeValidation'
+		);
+
+        //if existinguser doesn't exist
+        if (!existingUser) {
+			return res
+				.status(401)
+				.json({ success: false, message: 'User does not exists!' });
+		}
+
+        //if user is verified
+        if (existingUser.verified) {
+			return res
+				.status(400)
+				.json({ success: false, message: 'you are already verified!' });
+		}
+
+        if (
+			!existingUser.verificationCode ||
+			!existingUser.verificationCodeValidation
+		) {
+			return res
+				.status(400)
+				.json({ success: false, message: 'something is wrong with the code!' });
+		}
+
+        if (Date.now() - existingUser.verificationCodeValidation > 5 * 60 * 1000) {
+			return res
+				.status(400)
+				.json({ success: false, message: 'code has expired!' });
+		}
+
+        const hashedCodeValue = hmacProcess(
+			codeValue,
+			process.env.HMAC_VERIFICATION_CODE_SECRET
+		);
+		if (hashedCodeValue === existingUser.verificationCode) {
+			existingUser.verified = true;
+			existingUser.verificationCode = undefined;
+			existingUser.verificationCodeValidation = undefined;
+			await existingUser.save();
+			return res
+				.status(200)
+				.json({ success: true, message: 'your account has been verified!' });
+		}
+		return res
+			.status(400)
+			.json({ success: false, message: 'unexpected error occurred!!' });
+    }
+    catch(error){
         console.log(error);
     }
 }
