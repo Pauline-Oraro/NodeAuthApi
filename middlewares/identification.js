33d9e7047a29c7efb69e0f43926f33339c2c3a0b
Pauline-Oraro <hiltrapauline@gmail.com>
2025-08-20 17:14:16 +0300

    identifier middleware

--- middlewares/identification.js
+++ middlewares/identification.js
@@ -1,32 +1,27 @@
-const jwt = require ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 exports.identifier = (req, res, next) => {
-    let token; 
+	let token;
+	if (req.headers.client === 'not-browser') {
+		token = req.headers.authorization;
+	} else {
+		token = req.cookies['Authorization'];
+	}
 
-    if(req.headers.client === 'not-browser'){
-        token = req.headers.authorization
-    }else{
-        token = req.cookies['Authorization']
-    }
+	if (!token) {
+		return res.status(403).json({ success: false, message: 'Unauthorized' });
+	}
 
-    if(!token){
-        return res.status(403).json({ success: false, message: 'Authentication failed, token not found' });
-    }
-
-    try{
-        const userToken = token.split('')[1];
-
-        //verify the token with jwt
-        const jwtVerified = jwt.verify(userToken, process.env.TOKEN_SECRET);
-
-        if(jwtVerified){
-            req.user = jwtVerified;
-            next();
-        }else{
-            throw new Error('JWT verification failed');
-        }
-    }
-    catch (error){
-        console.log(error);
-    }
-}
+	try {
+		const userToken = token.split(' ')[1];
+		const jwtVerified = jwt.verify(userToken, process.env.TOKEN_SECRET);
+		if (jwtVerified) {
+			req.user = jwtVerified;
+			next();
+		} else {
+			throw new Error('error in the token');
+		}
+	} catch (error) {
+		console.log(error);
+	}
+};
